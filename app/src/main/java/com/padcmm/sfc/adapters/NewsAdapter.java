c94329889182831d81ad2a10f7413b24b096381b
kcns <k.channyeinsu@gmail.com>
2017-11-11 15:58:15 +0630

    Revert "Revert "Update and fix butterknife gradle version""
    This reverts commit 08e6264ce6ac2b4dccf0ed413bbef12f83076fc5.

---
+++ app/src/main/java/com/padcmm/sfc/adapters/NewsAdapter.java
@@ -0,0 +1,39 @@
+package com.padcmm.sfc.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.padcmm.sfc.R;
+import com.padcmm.sfc.viewholders.NewsViewHolder;
+
+/**
+ * Created by KCNS on 11/4/2017.
+ */
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsViewHolder> {
+
+    private LayoutInflater mLayoutInflater;
+
+    public NewsAdapter(Context context){
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View newsItemView = mLayoutInflater.inflate(R.layout.view_item_news, parent, false);
+        return new NewsViewHolder(newsItemView);
+    }
+
+    @Override
+    public void onBindViewHolder(NewsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 16;
+    }
+}
